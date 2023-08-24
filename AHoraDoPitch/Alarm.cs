f9c7f0c625b4d0d71590afba69232c15f8a6a439
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-08-24 17:59:18 -0300

    Implementado a Classe no programa principal.

--- AHoraDoPitch/Alarm.cs
+++ AHoraDoPitch/Alarm.cs
@@ -2,11 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AHoraDoPitch
 {
-    internal class Alarm
+    public class Alarm
     {
-    }
+		public Alarm() 
+		{ 
+			this.Tempo = 3; // três minutos
+		}
+
+		public Alarm(int tempo)
+		{
+			this.Tempo = tempo;
+		}
+
+		//controla o tempo do pitch
+		private int tempo;
+
+		public int Tempo
+		{
+			get { return this.tempo; }
+			set { this.tempo = 1000 * 60 * value; }
+		}
+
+		public void StartAlarm()
+		{
+			Thread.Sleep(this.Tempo);
+			Console.Beep(250,1000);
+		}
+
+	}
 }
--- AHoraDoPitch/Program.cs
+++ AHoraDoPitch/Program.cs
@@ -10,6 +10,23 @@ namespace AHoraDoPitch
     {
         static void Main(string[] args)
         {
+            Alarm alarme = new Alarm();
+            String resposta = "S";
+
+            while(resposta != "N")
+            {
+                Console.WriteLine("A Hora do Pitch");
+                Console.Write("Informe a duração do Pitch.: ");
+                int tempo = Convert.ToInt32(Console.ReadLine());
+
+                alarme.Tempo = tempo;
+                alarme.StartAlarm();
+
+                Console.WriteLine("Executar o programa novamente (S/N).: ");
+                resposta = Console.ReadLine().ToUpper();
+            }
+
+            
         }
     }
 }
